d66af119453d1ccf4dd7772cfd56d41b21fd5eca
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-06 11:07:39 +0200

    Configured logics to activate user with startcode, recieved back an access key, did a fast login-TEST (not permanant markup), seems to work fine

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\ActivateUserController;
 use App\Http\Controllers\ExchangeController;
 use App\Http\Controllers\StampController;
 use App\Http\Controllers\VictoryPointsController;
+use App\Http\Middleware\AccessKeyAuth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/stamps', [StampController::class, 'index']);
 Route::post('/stamps', [StampController::class, 'store']);
 Route::post('/exchanges', [ExchangeController::class, 'store']);
 Route::get('/victory-points', [VictoryPointsController::class, 'show']);
+Route::post('/activate', [ActivateUserController::class, 'store']);
+
+Route::get('/user', function (Request $request) {
+    return response()->json($request->user());
+})->middleware(AccessKeyAuth::class);
